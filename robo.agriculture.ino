16e83621f2e78401ea7a7b67526f3364a883f065
Shubhendu1Ghosh <100057852+Shubhendu1Ghosh@users.noreply.github.com>
2022-06-13 23:14:08 +0530

    frdr

---
+++ robo.agriculture.ino
@@ -0,0 +1,28 @@
+
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(2,3);
+#include<dht.h>
+#define dht_dpin 7; 
+dht DHT;
+char c=0;
+void setup() {
+ pinMode(8,OUTPUT);
+ Serial.begin(9600);
+}
+
+void loop() {
+  DHT.read11(dht_dpin);
+ float msval=analogRead(A0);
+float hmd=DHT.humidity;
+float temp=DHT.temperature;
+  if(mySerial.available()>0)
+  {
+    c = mySerial.read();
+    if(c == '1' ){
+     digitalWrite(8,HIGH); 
+    }
+    if(c == '2' ){
+     digitalWrite(8,LOW); 
+    }     
+  }
+}
